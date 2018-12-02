f882285d1f56662aa253c06312f6598ca984b275
11onefamilyteam22 <45063802+11onefamilyteam22@users.noreply.github.com>
2018-12-02 23:16:13 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,6 @@
 client.on("ready", () => {
   function lol() {
-    client.guilds.get('518678302282481684').roles.find("name", "Disco").setColor("RANDOM");
+    client.guilds.get('518874511249244180').roles.find("name", "Disco").setColor("RANDOM");
   };
   setInterval(lol, 10000);
 });
