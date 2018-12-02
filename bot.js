f772fdcde5121eec77aaba2ffbe0da20aa59f061
11onefamilyteam22 <45063802+11onefamilyteam22@users.noreply.github.com>
2018-12-02 23:49:26 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,8 +1,10 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
-const prefix = "#"
+const prefix = "$"
 client.on('ready', () => {
-  console.log(`Logged in as ${client.user.tag}!`);
+  console.log('---------------');
+  console.log(' Bot Is Online')
+  console.log('---------------')
 });
 
 
