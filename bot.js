cd123a70f1cbf453b8129ac40751a53b7eca1b5b
11onefamilyteam22 <45063802+11onefamilyteam22@users.noreply.github.com>
2018-12-02 23:20:30 +0200

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,6 +1,36 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+const prefix = "#"
+client.on('ready', () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+
+
+
+
+
+
+
+
+
 client.on("ready", () => {
   function lol() {
     client.guilds.get('518874511249244180').roles.find("name", "Disco").setColor("RANDOM");
   };
   setInterval(lol, 10000);
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+client.login(process.env.TOKEN);// لا تغير فيها شيء
